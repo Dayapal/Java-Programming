dc0ed7c04dab79bcb7bf5e338428c62ed062d149
Daya Pal <dayapal12pc@gmail.com>
2026-08-17 12:11:41 +0530

    Update age variable and improve driving eligibility message in LogicalOperators class

--- Day2/LogicalOperators.java
+++ Day2/LogicalOperators.java
@@ -1,11 +1,16 @@
 public class LogicalOperators {
     public static void main(String[] args) {
-        int age = 25;
+        int age = 1;
         boolean hasLicense = true;
 
         if (age >= 18 && hasLicense) {
             System.out.println("You can drive");
+        }else {
+            System.out.println("You cannot drive the car ");
         }
 
     }
 }
+
+
+
