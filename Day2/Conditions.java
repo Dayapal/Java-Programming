430fa3240692605c610ba3a15e08027f43c671a8
Daya Pal <dayapal12pc@gmail.com>
2026-08-17 11:04:12 +0530

    Add Conditions and Conditions2 classes with basic age eligibility logic

---
+++ Day2/Conditions.java
@@ -0,0 +1,11 @@
+public class Conditions{
+    public static void main(String[] args){
+        int age = 11;
+        if(age >=18){
+            System.out.println("You are eligible for vote");
+        }else{
+            System.out.println("You are not eligible for vote you are under 18");
+        }
+    }
+
+}
