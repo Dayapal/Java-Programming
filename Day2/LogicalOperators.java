f4fcb51f5fe1b89908025ba70907499ba0174668
Daya Pal <dayapal12pc@gmail.com>
2026-08-17 15:26:43 +0530

    Refactor LogicalOperators class for improved readability and add TernaryOperator class for age evaluation

--- Day2/LogicalOperators.java
+++ Day2/LogicalOperators.java
@@ -1,16 +1,23 @@
 public class LogicalOperators {
     public static void main(String[] args) {
         int age = 1;
+        
+    
         boolean hasLicense = true;
-
         if (age >= 18 && hasLicense) {
             System.out.println("You can drive");
-        }else {
+        } else {
             System.out.println("You cannot drive the car ");
         }
 
+        boolean isRaining = true;
+        if(!isRaining){
+            System.out.println("Go outside");
+        }else{
+            System.out.println("You cannot go outside");
+        }
+        
+        
+
     }
 }
-
-
-
---
+++ Day2/TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        int age = 21;
+        String ans = age >= 18 ? "Adult" : "child";
+        System.out.printf( "Result" , ans);
+
+    }
+}
