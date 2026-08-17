0cec0ca205a1faecc83c969a04b11a4c031616dc
Daya Pal <dayapal12pc@gmail.com>
2026-08-17 11:22:22 +0530

    Add ComparisonOperators and LogicalOperators classes with basic comparison and logical operations

---
+++ Day2/LogicalOperators.java
@@ -0,0 +1,13 @@
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        int age = 25;
+        boolean hasLicense = true;
+
+        if (age >= 18 && hasLicense) {
+            System.out.println("You can drive");
+        }
+
+    }
+}
