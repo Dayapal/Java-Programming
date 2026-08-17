e8eb22122915dd07ab603b98e5e760a060df58cb
Daya Pal <dayapal12pc@gmail.com>
2026-08-17 11:16:24 +0530

    Add Nested class with age verification logic

---
+++ Day2/Nested.java
@@ -0,0 +1,18 @@
+public class Nested {
+
+    public static void main(String[] args) {
+        int age = 20;
+        boolean hasId = true;
+
+        if (age >= 18) {
+
+            if (hasId) {
+                System.out.println("Entry allowed");
+            }
+
+        } else {
+            System.out.println("You are under 18");
+        }
+
+    }
+}
