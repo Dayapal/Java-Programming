2251bada3368584cb7e158d51249a916b04c3dfe
Daya Pal <dayapal12pc@gmail.com>
2026-08-18 11:01:48 +0530

    Add PracticeJava class with number evaluation logic

---
+++ Day2/PracticeJava.java
@@ -0,0 +1,16 @@
+public class PracticeJava {
+    public static void main(String[] args) {
+        int number = 21;
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+        int num1 = 21;
+        int num2 = 30;
+        if (num1 > num2) {
+             System.out.println("Num1 is greater than num2");
+        }
+    }
+
+}
