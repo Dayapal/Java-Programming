24706eefbe68ccf5e00a522673cbbcb5dfc232e2
Daya Pal <dayapal12pc@gmail.com>
2026-08-20 11:02:07 +0530

    Add InputTaking class to handle user input and calculate the sum of two numbers

---
+++ Day5/inputTaking/InputTaking.java
@@ -0,0 +1,20 @@
+package Day5.inputTaking;
+
+import java.util.Scanner;
+
+public class InputTaking {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your First Number");
+        int a = scn.nextInt();
+
+        System.out.println("Enter you Second Number: ");
+        int b = scn.nextInt();
+
+        System.out.println("Sum " + (a + b));
+        scn.close();
+
+    }
+
+}
