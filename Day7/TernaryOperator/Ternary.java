ac45ccd791ea3538d1b92bb535102cc2fbbd4d86
Daya Pal <dayapal12pc@gmail.com>
2026-08-22 10:37:06 +0530

    Add number parity check to Ternary class using the ternary operator

--- Day7/TernaryOperator/Ternary.java
+++ Day7/TernaryOperator/Ternary.java
@@ -6,6 +6,7 @@ public class Ternary {
         String result = "";
 
         int age = 71;
+        int number = 21;
 
         if (age >= 21) {
             result = "Adult";
@@ -16,5 +17,10 @@ public class Ternary {
 
         String result2 = age >= 21 ? "Adult" : "Minor";
         System.out.println("Result 2 answer: " + result2);
+
+        String answer = number%2==0?"Even": "Odd";
+        System.out.println("Number is odd or even : "+ answer);
+
+        
     }
 }
