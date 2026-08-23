152939efd2909775b1dfeb16279df61715d0f09d
Daya Pal <dayapal12pc@gmail.com>
2026-08-23 15:49:28 +0530

    Refactor Positive class to include user input for number positivity check

--- Day8/Positive.java
+++ Day8/Positive.java
@@ -1,5 +1,21 @@
 package Day8;
 
+import java.util.Scanner;
+
 public class Positive {
-    
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
 }
