6233845445f74fcc4446540186512d0a8d7fa4ca
Daya Pal <dayapal12pc@gmail.com>
2026-08-26 10:06:32 +0530

    Remove unnecessary whitespace in Main class

--- Day8/Main.java
+++ Day8/Main.java
@@ -4,12 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter number: ");
         int number = sc.nextInt();
-
         if (number % 5 == 0 && number % 11 == 0) {
             System.out.println("Divisible by both 5 and 11");
         } else {
