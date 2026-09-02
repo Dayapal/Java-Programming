d9f94fa463264652e628261892df204b723ed771
Daya Pal <dayapal12pc@gmail.com>
2026-09-02 10:59:08 +0530

    Add greet method to MethodParameters class

--- Day11/MethodParameters.java
+++ Day11/MethodParameters.java
@@ -2,4 +2,16 @@ package Day11;
 
 public class MethodParameters {
     
+
+    static void greet(String name){
+        System.out.println("Hello Good Morning " + name);
+    };
+
+    public static void main(String[] args){
+        greet("Daya");
+        greet("Rahul");
+        greet("Ramesh");
+    }
+
+    
 }
