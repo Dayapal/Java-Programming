b1d614c0d852d1002dc0ab6df51c1c2e90f2e9b8
Daya Pal <dayapal12pc@gmail.com>
2026-09-02 10:54:07 +0530

    Add BasicMethod, Function, and MethodParameters classes

---
+++ Day11/BasicMethod.java
@@ -0,0 +1,16 @@
+package Day11;
+
+public class BasicMethod {
+     static void sayHello() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+
+        sayHello();
+        sayHello();
+        sayHello();
+
+    }
+    
+}
---
+++ Day11/Function.java
@@ -0,0 +1,15 @@
+package Day11;
+
+public class Function {
+    // In Java, functions are usually called methods.
+    // Methods allow you to:
+    // Reuse code
+    // Break a large program into smaller pieces
+    // Avoid repeating the same code
+    // Make programs easier to understand
+    // Prepare for OOP and DSA
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+}
