9b540c7c836950cb89588b5f996dcf3c86227942
Daya Pal <dayapal12pc@gmail.com>
2026-09-03 10:42:16 +0530

    Remove unnecessary blank line in Login class

--- Day12/Login.java
+++ Day12/Login.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         String correctEmail = "admin@gmail.com";
         String correctpassword = "12345";
 
