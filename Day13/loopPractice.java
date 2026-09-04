a24af820195e3997c7de56211df455b34c44fe55
Daya Pal <dayapal12pc@gmail.com>
2026-09-04 08:45:33 +0530

    Add Loop and loopPractice classes for basic iteration examples

---
+++ Day13/loopPractice.java
@@ -0,0 +1,11 @@
+package Day13;
+public  class loopPractice{
+    public  static  void main(String[] args){
+        for(int i=1; i <= 20; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+
+}
