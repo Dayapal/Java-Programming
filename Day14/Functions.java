8294dc37a68f6b3853ac60f53a51b004fe0d2357
Daya Pal <dayapal12pc@gmail.com>
2026-09-05 10:48:07 +0530

    Remove unused arithmetic methods and user input handling from Functions class

--- Day14/Functions.java
+++ Day14/Functions.java
@@ -1,42 +1,14 @@
 package Day14;
-import java.util.Scanner;
-
 
 public class Functions {
-
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public static int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public static int divide(int a, int b) {
-        return a / b;
-    }
-
     public static void main(String[] args) {
+        printNumbers(5);
+    }
 
-        Scanner sc = new Scanner(System.in);
+    public static void printNumbers(int n) {
 
-        System.out.print("Enter a: ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter b: ");
-        int b = sc.nextInt();
-
-        System.out.println("Addition = " + add(a, b));
-        System.out.println("Subtraction = " + subtract(a, b));
-        System.out.println("Multiplication = " + multiply(a, b));
-
-        if (b != 0) {
-            System.out.println("Division = " + divide(a, b));
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
         }
-    
     }
 }
