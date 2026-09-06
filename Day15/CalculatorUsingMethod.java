09086c507aa52a6d30cf1abddeedce23f75d82a5
Daya Pal <dayapal12pc@gmail.com>
2026-09-06 09:11:56 +0530

    Add CalculatorUsingMethod class with basic addition functionality

---
+++ Day15/CalculatorUsingMethod.java
@@ -0,0 +1,13 @@
+package Day15;
+
+public class CalculatorUsingMethod {
+    public static void main(String[] args){
+
+       System.out.println("Addtion "+ add(50, 23));
+
+    }
+    public static int add(int a, int b){
+        return a + b;
+
+    }
+}
