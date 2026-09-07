023a9505f21fd7758bc97614b69e526aa5676e53
Daya Pal <dayapal12pc@gmail.com>
2026-09-07 10:40:27 +0530

    Add Squre class with method to calculate the square of a number

---
+++ Day16/Squre.java
@@ -0,0 +1,12 @@
+package Day16;
+
+public class Squre {
+    public static void main(String[] args) {
+        int number = Square(8);
+        System.out.println(number);
+    }
+
+    public static int Square(int number) {
+        return number * number;
+    }
+}
