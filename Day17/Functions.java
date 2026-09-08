002af0b1fe5232fbeecd73fd13cec282b6ea9f85
Daya Pal <dayapal12pc@gmail.com>
2026-09-08 10:18:54 +0530

    Add Pattern and Functions classes with basic functionality

---
+++ Day17/Functions.java
@@ -0,0 +1,10 @@
+package Day17;
+public  class Functions{
+    public  static int add(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args){
+          int result = add(5,2);
+          System.out.println(result);
+    }
+}
