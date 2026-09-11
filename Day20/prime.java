46d6765ad4ea2cec3e7ef24201a84008be9da2f2
Daya Pal <dayapal12pc@gmail.com>
2026-09-11 11:37:44 +0530

    Fix prime number checker logic in prime.java

--- Day20/prime.java
+++ Day20/prime.java
@@ -2,17 +2,17 @@ package Day20;
 
 public class prime {
     public static void main(String[] args) {
-        int number = 3;
+        int number = 6;
         int count = 0;
-        for(int i = 0; i <=number; i++){
-            if(i%number==0){
+        for(int i = 1; i <=number; i++){
+            if(number%i==0){
                 count += 1;
             }
         }
-        if(count >=2){
+        if(count == 2){
             System.out.println("This number is prime number");
         }else{
-            System.out.println("It is not a prime number")
+            System.out.println("It is not a prime number");
         }
     }
 }
