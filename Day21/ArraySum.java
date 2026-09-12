cf5d8059e9b042e1b7dc1a6864f8707735588d4c
Daya Pal <dayapal12pc@gmail.com>
2026-09-12 10:29:00 +0530

    Implement ArraySum class with main method to calculate and display the sum of a predefined array

--- Day21/ArraySum.java
+++ Day21/ArraySum.java
@@ -1,5 +1,13 @@
 package Day21;
 
 public class ArraySum {
-    
+    public static void main(String[] args){
+        int[] array = {10,20,30,40,50};
+
+        int sum = 0;
+        for(int i = 0; i  < array.length; i++){
+            sum += array[i];
+        }
+        System.out.println("Sum of array : "+ sum);
+    }
 }
