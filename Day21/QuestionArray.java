8e98c2c82d06949b16c7f0ede0da63563656d525
Daya Pal <dayapal12pc@gmail.com>
2026-09-12 10:26:38 +0530

    Refactor output format in QuestionArray class and add ArraySum class skeleton

--- Day21/QuestionArray.java
+++ Day21/QuestionArray.java
@@ -2,7 +2,6 @@ package Day21;
 public class QuestionArray {
     public static void main(String[] args){
        int[] array = {1,2,3,4,5,6};
-
-       System.out.println(array.length);
+       System.out.println( "Length  " + array.length);
     }
 }
