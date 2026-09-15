c31da653448aeb8e8fb20056e8dd67bab1ca9d58
Daya Pal <dayapal12pc@gmail.com>
2026-09-15 10:21:29 +0530

    Add Minimum class to find and display the minimum value in an array

---
+++ Day23/Minimum.java
@@ -0,0 +1,13 @@
+package Day23;
+
+public class Minimum {
+    public static void main(String[] args) {
+        int[] arr = {102,20,30,40,50};
+        int min = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+
+        }
+    }
+    
+}
