a2c006e2b35d2659e88752ea5eb4f1590a30502e
Daya Pal <dayapal12pc@gmail.com>
2026-09-16 10:16:34 +0530

    Add TakingInput class to handle user input for array size

---
+++ Day24/TakingInput.java
@@ -0,0 +1,13 @@
+package Day24;
+
+import java.util.Scanner;
+
+public class TakingInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+
+    }
+}
