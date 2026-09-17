862dc5f0cebf8bfd4ddaf846a69bac40d32dbf37
Daya Pal <dayapal12pc@gmail.com>
2026-09-17 10:42:58 +0530

    Refactor TwoDArray class to correct variable names and improve output display

--- Day25/TwoDArray.java
+++ Day25/TwoDArray.java
@@ -2,13 +2,15 @@ package Day25;
 
 public class TwoDArray {
     public static void main(String[] args) {
-        int[][] arr = {
+        int[] SingleArray = {10,20,30,40,50};
+        int[][] Twoarr = {
             {10,20,30,50,60},
             {10,20,30,50,60},
             {70,80,90,100,110},
             {70,80,90,100,110},
             {70,80,90,100,110},
         };
-        System.out.println("TwoD array : " + arr);
+        System.out.println("TwoD array : " + Twoarr);
+
     }
 }
