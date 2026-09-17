9370298859d22f0fefb622d6e20ac67d2da0d2a6
Daya Pal <dayapal12pc@gmail.com>
2026-09-17 10:40:27 +0530

    Add TwoDArray class to demonstrate 2D array initialization and output

---
+++ Day25/TwoDArray.java
@@ -0,0 +1,14 @@
+package Day25;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {10,20,30,50,60},
+            {10,20,30,50,60},
+            {70,80,90,100,110},
+            {70,80,90,100,110},
+            {70,80,90,100,110},
+        };
+        System.out.println("TwoD array : " + arr);
+    }
+}
