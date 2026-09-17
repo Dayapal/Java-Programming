14f2d99621209d205d51e0908fdab89539a26ba8
Daya Pal <dayapal12pc@gmail.com>
2026-09-17 10:49:00 +0530

    Improve formatting and output display in TwoDArray class

--- Day25/TwoDArray.java
+++ Day25/TwoDArray.java
@@ -1,16 +1,18 @@
 package Day25;
 
+import java.util.Arrays;
+
 public class TwoDArray {
     public static void main(String[] args) {
-        int[] SingleArray = {10,20,30,40,50};
+        int[] SingleArray = { 10, 20, 30, 40, 50 };
         int[][] Twoarr = {
-            {10,20,30,50,60},
-            {10,20,30,50,60},
-            {70,80,90,100,110},
-            {70,80,90,100,110},
-            {70,80,90,100,110},
+                { 10, 20, 30, 50, 60 },
+                { 10, 20, 30, 50, 60 },
+                { 70, 80, 90, 100, 110 },
+                { 70, 80, 90, 100, 110 },
+                { 70, 80, 90, 100, 110 },
         };
-        System.out.println("TwoD array : " + Twoarr);
+        System.out.println("TwoD array : " + Arrays.deepToString(Twoarr));
         System.out.println("Single Array " + SingleArray);
 
     }
