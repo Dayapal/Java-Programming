6f99e4aad5c1068b2e6221dff2895535e49f07b7
Daya Pal <dayapal12pc@gmail.com>
2026-09-18 10:29:24 +0530

    Add loop to print all characters of the name string in Index class

--- Day26/Index.java
+++ Day26/Index.java
@@ -5,5 +5,8 @@ public class Index {
         String name = "Raja";
         
         System.out.println(name.charAt(3));
+        for(int i = 0; i < name.length(); i++){
+            System.out.println(name.charAt(i));
+        }
     }
 }
