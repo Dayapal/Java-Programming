2624a236b751bbf13b92b02493176e77d1fc9e20
Daya Pal <dayapal12pc@gmail.com>
2026-09-19 10:18:48 +0530

    Remove unnecessary blank line in Index class main method

--- Day26/Index.java
+++ Day26/Index.java
@@ -3,7 +3,6 @@ package Day26;
 public class Index {
     public static void main(String[] args) {
         String name = "Raja";
-        
         System.out.println(name.charAt(3));
         for(int i = 0; i < name.length(); i++){
             System.out.println(name.charAt(i));
