72b184068a2a0cd0de382bade7c18e82305dfcc7
Daya Pal <dayapal12pc@gmail.com>
2026-09-19 10:32:59 +0530

    Add Index and Travers classes to demonstrate string manipulation and character access

---
+++ Day27/Index.java
@@ -0,0 +1,11 @@
+package Day27;
+
+public class Index {
+    public static  void main(String[] args){
+        String a = new String("java");
+        String b = new String("java");
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+    }
+    
+}
---
+++ Day27/Travers.java
@@ -0,0 +1,17 @@
+package Day27;
+
+public class Travers {
+    public static void main(String[] args) {
+        String str = new String("HelloEveryone");
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            System.out.println(ch);
+        }
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+        
+    }
+}
